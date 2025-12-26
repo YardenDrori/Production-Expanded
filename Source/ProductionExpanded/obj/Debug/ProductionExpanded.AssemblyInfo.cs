3759f303cae0b46f5f82448379e6bcbae08d0531
YardenDrori <yarden.drori.yd@gmail.com>
2025-12-26 11:17:09 +0200

    minor tweaks (yeah srsly)

--- Source/ProductionExpanded/obj/Debug/ProductionExpanded.AssemblyInfo.cs
+++ Source/ProductionExpanded/obj/Debug/ProductionExpanded.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8e155f8fcb9d4ea5a831887c2065eb1536eef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
