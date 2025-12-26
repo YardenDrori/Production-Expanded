3b37bbe626acf04777703f3a67be0cc618a25584
YardenDrori <yarden.drori.yd@gmail.com>
2025-12-26 15:19:06 +0200

    buncha stuff tbh

--- Source/ProductionExpanded/CompResourceProcessor.cs
+++ Source/ProductionExpanded/CompResourceProcessor.cs
@@ -25,17 +25,29 @@ namespace ProductionExpanded
         private CompProperties_ResourceProcessor Props =>
             (CompProperties_ResourceProcessor)props;
         private bool isProcessing = false;
+        private bool isFinished = false;
         private int progressTicks = 0;
         private int totalTicksPerCycle = 0;
         private int cycles = 1;
         private int currentCycle = 0;
         private int inputCount = 0;
+        private int outputCount = 0;
         private ThingDef inputType = null;
         private ThingDef outputType = null;
+        private CompPowerTrader powerTrader = null;
+        private CompRefuelable refuelable = null;
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             base.PostSpawnSetup(respawningAfterLoad);
+            powerTrader = parent.GetComp<CompPowerTrader>();
+            refuelable = parent.GetComp<CompRefuelable>();
+
+            // Validate building configuration - log once on spawn
+            if (powerTrader == null && refuelable == null)
+            {
+                Log.Warning($"[Production Expanded] {parent.def.defName} has CompResourceProcessor but no CompPowerTrader or CompRefuelable.");
+            }
         }
 
         public override void CompTickRare()
@@ -43,18 +55,74 @@ namespace ProductionExpanded
             base.CompTickRare();
             if (isProcessing)
             {
-                progressTicks += 250;
-
-                //temp complete processing
-                if (progressTicks >= totalTicksPerCycle)
+                if (CanContinueProcessing())
                 {
-                    CompleteProcessingCycle();
+                    progressTicks += 250;
+
+                    //temp complete processing
+                    if (progressTicks >= totalTicksPerCycle)
+                    {
+                        CompleteProcessingCycle();
+                    }
+                }
+                else
+                {
+                    // remove progress if the building is unfueled/powered
+                    if (progressTicks > 750)
+                    {
+                        progressTicks -= 750;
+                    }
+                    else
+                    {
+                        progressTicks = 0;
+                    }
                 }
             }
             else
             {
                 //todo
             }
+        }
+
+        public bool CanContinueProcessing()
+        {
+            // Check power requirement if building has power
+            if (powerTrader != null && !powerTrader.PowerOn)
+            {
+                return false; //no power
+            }
+
+            // Check fuel requirement if building has fuel
+            if (refuelable != null && !refuelable.HasFuel)
+            {
+                return false; // no fuel
+            }
+
+            // If we get here, either:
+            // - Building has power AND it's on
+            // - Building has fuel AND it has fuel
+            return true; // we gucci
+        }
+
+        public void StartProcessing(ThingDef inputType, ThingDef outputType, int cycles, int ticksPerItem, int inputCount, int outputCount)
+        {
+            if (isProcessing || isFinished)
+            {
+                return;
+            }
+            isProcessing = true;
+            progressTicks = 0;
+            currentCycle = 0;
+            totalTicksPerCycle = ticksPerItem * inputCount;
+            if (totalTicksPerCycle < ticksPerItem * 10)
+            {
+                totalTicksPerCycle = ticksPerItem * 10;
+            }
+            this.inputType = inputType;
+            this.outputType = outputType;
+            this.cycles = cycles;
+            this.inputCount = inputCount;
+            this.outputCount = outputCount;
         }
 
         public void CompleteProcessingCycle()
@@ -64,11 +132,30 @@ namespace ProductionExpanded
             if (currentCycle >= cycles)
             {
                 isProcessing = false;
+                isFinished = true;
                 inputCount = 0;
                 inputType = null;
-                outputType = null;
                 currentCycle = 0;
                 totalTicksPerCycle = 0;
+                cycles = 0;
+            }
+        }
+
+        public void EmptyBuilding()
+        {
+            if (isFinished)
+            {
+                // Create the output item
+                Thing item = ThingMaker.MakeThing(outputType);
+                item.stackCount = outputCount;
+
+                // Spawn it at the building's interaction cell
+                GenSpawn.Spawn(item, parent.InteractionCell, parent.Map);
+
+                // Reset state
+                isFinished = false;
+                outputType = null;
+                outputCount = 0;
             }
         }
 
@@ -90,9 +177,17 @@ namespace ProductionExpanded
         public override void PostExposeData()
         {
             base.PostExposeData();
+            base.PostExposeData();
 
-            // In Step 2, we'll save/load our state here
-            // For now, nothing to save (we have no state)
+            // Save/load all our state variables
+            Scribe_Values.Look(ref isProcessing, "isProcessing", false);
+            Scribe_Values.Look(ref progressTicks, "progressTicks", 0);
+            Scribe_Values.Look(ref totalTicksPerCycle, "totalTicksPerCycle", 0);
+            Scribe_Values.Look(ref cycles, "cycles", 1);
+            Scribe_Values.Look(ref currentCycle, "currentCycle", 0);
+            Scribe_Values.Look(ref inputCount, "inputCount", 0);
+            Scribe_Defs.Look(ref inputType, "inputType");
+            Scribe_Defs.Look(ref outputType, "outputType");
         }
 
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
