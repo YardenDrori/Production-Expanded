c51a2026c2b06fe7e0d985ce2d58ab0ce98f32a6
Random Internet Person <yarden.drori.yd@gmail.com>
2025-12-29 14:06:27 +0200

    added devilstrand and added recipes to electric tailor bench

--- Source/ProductionExpanded/obj/Debug/ProductionExpanded.AssemblyInfo.cs
+++ Source/ProductionExpanded/obj/Debug/ProductionExpanded.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2227af6a050297094cc7cc6f0e66935ffea8e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a37a8411fa418beac0124ffef1ce09dd2c8cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductionExpanded")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
